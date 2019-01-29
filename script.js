4ca0072595564251a66718ba0d3be4b6c06c1acf
elaintran <elain.ttran@gmail.com>
2019-01-28 22:10:13 -0600

    keys

---
+++ script.js
@@ -0,0 +1,47 @@
+var keys = [{
+    keyCode: 81,
+    keyTrigger: 'Q',
+    id: 'clap',
+    url: 'https://raw.githubusercontent.com/wesbos/JavaScript30/master/01%20-%20JavaScript%20Drum%20Kit/sounds/clap.wav'
+  }, {
+    keyCode: 87,
+    keyTrigger: 'W',
+    id: 'hihat',
+    url: 'https://raw.githubusercontent.com/wesbos/JavaScript30/master/01%20-%20JavaScript%20Drum%20Kit/sounds/hihat.wav'
+  }, {
+    keyCode: 69,
+    keyTrigger: 'E',
+    id: 'kick',
+    url: 'https://raw.githubusercontent.com/wesbos/JavaScript30/master/01%20-%20JavaScript%20Drum%20Kit/sounds/kick.wav'
+  }, {
+    keyCode: 65,
+    keyTrigger: 'A',
+    id: 'openhat',
+    url: 'https://raw.githubusercontent.com/wesbos/JavaScript30/master/01%20-%20JavaScript%20Drum%20Kit/sounds/openhat.wav'
+  }, {
+    keyCode: 83,
+    keyTrigger: 'S',
+    id: 'boom',
+    url: 'https://raw.githubusercontent.com/wesbos/JavaScript30/master/01%20-%20JavaScript%20Drum%20Kit/sounds/boom.wav'
+  }, {
+    keyCode: 68,
+    keyTrigger: 'D',
+    id: 'ride',
+    url: 'https://raw.githubusercontent.com/wesbos/JavaScript30/master/01%20-%20JavaScript%20Drum%20Kit/sounds/ride.wav'
+  }, {
+    keyCode: 90,
+    keyTrigger: 'Z',
+    id: "snare",
+    url: 'https://raw.githubusercontent.com/wesbos/JavaScript30/master/01%20-%20JavaScript%20Drum%20Kit/sounds/snare.wav'
+  }, {
+    keyCode: 88,
+    keyTrigger: 'X',
+    id: 'tom',
+    url: 'https://raw.githubusercontent.com/wesbos/JavaScript30/master/01%20-%20JavaScript%20Drum%20Kit/sounds/tom.wav'
+  }, {
+    keyCode: 67,
+    keyTrigger: 'C',
+    id: 'tink',
+    url: 'https://raw.githubusercontent.com/wesbos/JavaScript30/master/01%20-%20JavaScript%20Drum%20Kit/sounds/tink.wav'
+  },
+]
